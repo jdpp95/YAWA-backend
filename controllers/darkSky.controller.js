2709578d7ce702a38e9685b7d93a98aa89ff14be
jdpp95 <jdporrasp@unal.edu.co>
2021-07-07 10:34:51 -0500

    Dark sky hardcoded request

--- controllers/darkSky.controller.js
+++ controllers/darkSky.controller.js
@@ -1,5 +1,20 @@
+const DarkSky = require('dark-sky')
+const darksky = new DarkSky(process.env.DARK_SKY)
+
 const darkSkyTest = async (req, res) => {
-    res.json("Hello world!")
+
+    darksky
+        .latitude(4.6116)
+        .longitude(-74.2069)
+        .time(req.query.time)
+        .units('si')
+        .get()
+        .then(r => {
+            res.json(r)
+        })
+        .catch(console.log)
+
+    
 }
 
 module.exports = {
