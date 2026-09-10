4ce955137c333f77926fc22ee392e4f926d1b2fb
jdpp95 <jdporrasp@gmail.com>
2026-09-09 23:32:24 -0500

    Adjust Fly networking configuration

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const dns = require('dns');
+
+const originalLookup = dns.lookup;
+dns.lookup = function (hostname, options, callback) {
+  // if (hostname === 'api.open-meteo.com') {
+  //   const targetCallback = typeof options === 'function' ? options : callback;
+  //   return targetCallback(null, '188.40.99.226', 4); 
+  // }
+  return originalLookup.apply(this, arguments);
+};
+
 require('dotenv').config();
 const Server = require('./models/server');
 
